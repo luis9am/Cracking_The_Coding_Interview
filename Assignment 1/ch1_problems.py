0fe4a3be683e3ee79ac2e6cc8d52095fa337efb3
Luis A Mireles <luui9mirel@gmail.com>
2018-05-18 00:00:01 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Project.py
    #	Project_Space_Manager.py

--- Assignment 1/ch1_problems.py
+++ Assignment 1/ch1_problems.py
@@ -70,10 +70,68 @@ def chk_permutation(string1, string2):
         return False
 
 
-def main():
+# 1.3 urlify:
+#
+# Write a method to replace all spaces in a string with '%20: You may assume that the string
+# has sufficient space at the end to hold the additional characters, and that you are given the "true"
+# length of the string. (Note: If implementing in Java, please use a character array so that you can
+# perform this operation in place.)
+#
+# Input: "Mr John Smith "J 13
+# Output: "Mr%20John%20Smith"
+#
+# I   : Analyze string and input 20% at every ' ', make sure there's no trailing space at end
+# D   : Should return previous string with every space as percent with no trailing space
+# E/A : Similar to previous examples we scan string then manipulate each space to hold percent.
+#       having done this, we then make sure we don't replace trailing spaces and instead remove it
+#       with percents and just omit them from our end goal
+# L   : Since strings are immutable in python we convert it to a list and analyze string in reverse.
+#       since we have an extra buffer at the end it allows us to change chars without overwriting.
 
-    is_unique("dayzz")  # 1.1
-    chk_permutation("strasd", "string")  # 1.2
+def urlify(in_string, length):
+    in_string = list(in_string)
+    # enumerate string and scan in reversed order for instances of ' '
+    for i, character in reversed(list(enumerate(in_string[0:length]))):
+        if character == ' ':
+            in_string[i + 3:length + 2] = in_string[i + 1:length]
+            in_string[i:i + 3] = '%20'
+            length += 2
+    return ''.join(in_string)
+
+
+# 1.4 Palindrome Permutation:
+# Given a string, write a function to check if it is a permutation of
+# a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A
+# permutation is a rearrangement of letters. The palindrome does not need to be limited to just
+# dictionary words.
+#
+# Input: Tact Coa
+# Output: True (permutations:"taco cat'; "atco cta'; etc.)
+#
+# I   : Asking for a permutation of a palindrome..
+# D   : Return true or false
+# E/A : So, palindromes are sets of words that can be mirrored from start to mid and mid to end.
+#       This characteristic allows us to check that if a word contains less than 3 letters it is not a
+#       palindrome. Since now, it can also be a permutation of any set of letters my strategy is to
+#       analyze the letters and count each occurrence. Occurrences for letters should all equal 2 and at
+#       most 1 occurrence of another letter that will be the middle letter if odd, else fails.
+# L   :
+
+def palindrome_perm(string):
+    if len(string) < 3:
+        return "not palindrome"
+    else:
+        for i in str:
+            str[i] += 1
+
+
+
+
+def main():
+    print("Testing:\n\n")
+    # is_unique("dayzz")  # 1.1
+    # chk_permutation("strasd", "string")  # 1.2
+    # print(urlify("Mr John Smith ", 13))
 
 
 if __name__ == "__main__":
