239fdbd47c97e0aab5e4f6e90b289bad8acdbdb4
Luis A Mireles <luui9mirel@gmail.com>
2018-05-19 14:12:50 -0600

    Completed question 1.5

---
+++ Assignment 1/ch2_problems.py
@@ -0,0 +1,92 @@
+
+
+#
+#   My Solutions to Cracking the Coding interview v.6
+#   Luis A. Mireles / Summer 2018
+#
+
+
+# 2.1 Remove Dups:
+# Write code to remove duplicates from an unsorted linked list.
+#
+# FOLLOW UP
+# How would you solve this problem if a temporary buffer is not allowed?
+#
+# I   :
+# D   :
+# E\A :
+# L   :
+
+def remove_dup():
+    print("..")
+
+
+# 2.2 Return Kth to Last:
+# Implement an algorithm to find the kth to last element of a singly linked list.
+#
+# I   :
+# D   :
+# E\A :
+# L   :
+
+def kth_from_last():
+    print("..")
+
+
+# 2.3 Delete Middle Node:
+# Implement an algorithm to delete a node in the middle (i.e., any node but
+# the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+# that node.
+#
+# Input: the node c from the linked list a - >b- >c - >d - >e- >f
+# Result: nothing is returned, but the new linked list looks like a - >b- >d - >e- >f
+#
+# I   :
+# D   :
+# E\A :
+# L   :
+
+def del_med():
+    print("..")
+
+
+# 2.4 Partition:
+# Write code to partition a linked list around a value x, such that all nodes less than x come
+# before all nodes greater than or equal to x. lf x is contained within the list, the values of x only need
+# to be after the elements less than x (see below). The partition element x can appear anywhere in the
+# "right partition"; it does not need to appear between the left and right partitions.
+#
+# Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5)
+# Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+# Hints: #3, #24
+#
+# I   :
+# D   :
+# E\A :
+# L   :
+
+def kth_from_last():
+    print("..")
+
+
+# 2.5 Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+# digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a
+# function that adds the two numbers and returns the sum as a linked list.
+#
+# Input: (7-) 1 -) 6) + (5 -) 9 -) 2) .Thatis,617 + 295.
+# Output: 2 -) 1 -) 9. That is, 912.
+#
+# FOLLOW UP
+# Suppose the digits are stored in forward order. Repeat the above problem.
+#
+# Input: (6 -) 1 -) 7) + (2 -) 9 -) 5).Thatis,617 + 295.
+# Output: 9 -) 1 -) 2. That is, 912.
+#
+# I   :
+# D   :
+# E\A :
+# L   :
+
+def sum_lists():
+    print("..")
+
